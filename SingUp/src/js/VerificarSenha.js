4bdb35f43ef6d67f7a0206658ba37856f08b8a44
Pedro Lucas <pedrooluucas2010@gmail.com>
2023-07-10 22:57:28 -0300

    Implemetando Java Script

---
+++ SingUp/src/js/VerificarSenha.js
@@ -0,0 +1,12 @@
+ 
+
+function confereSenha() {
+    const senha = document.querySelector('input[name=senha]');
+    const comfirmar = document.querySelector('input[name=comfirmar]');
+
+    if (comfirmar.value === senha.value) {
+        comfirmar.setCustomValidity('');
+    } else {
+        comfirmar.setCustomValidity('As Senhas não Conferem');
+    }
+}
